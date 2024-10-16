cbef6f7c956190985fa7cb9620a352b84a04892c
Alex31TheDev <alexdraghia45@gmail.com>
2024-10-16 20:57:26 +0300

    Update jest config

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+const config = {
+    testEnvironment: "node",
+    verbose: true,
+    transform: {},
+    roots: ["test"],
+    testPathIgnorePatterns: ["/node_modules/"],
+    moduleFileExtensions: ["js", "json", "node"]
+};
+
+export default config;
