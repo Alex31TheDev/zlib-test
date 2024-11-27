82abbdd6b13d9c6b5f5c48982d2858425c2ae5ac
Alex31TheDev <alexdraghia45@gmail.com>
2024-11-27 21:02:39 +0200

    More benchmark function to util

--- benchmark/base64.js
+++ benchmark/base64.js
@@ -10,34 +10,12 @@ const data = Array(dataLength)
 
 const encoded = Util.encodeBase64(data);
 
-function benchmark(name, func, data) {
-    let min = Number.MAX_VALUE,
-        max = 0,
-        sum = 0;
-
-    for (let i = 1; i <= runs; i++) {
-        const t1 = performance.now();
-
-        func(data);
-
-        const t2 = performance.now(),
-            time = t2 - t1;
-
-        min = Math.min(min, time);
-        max = Math.max(max, time);
-        sum += time;
-    }
-
-    const avg = sum / runs;
-    console.log(`${name}:`, "min:", min, "max:", max, "avg:", avg);
-}
-
 console.log("Data length:", dataLength, "runs:", runs, "\n");
 
-benchmark("Built-in encode", Util.encodeBase64, data);
-benchmark("Built-in decode", Util.decodeBase64, encoded);
+Util.benchmark("Built-in encode", runs, Util.encodeBase64, data);
+Util.benchmark("Built-in decode", runs, Util.decodeBase64, encoded);
 
 console.log();
 
-benchmark("Custom encode", Base64.encode, data);
-benchmark("Custom decode", Base64.decode, encoded);
+Util.benchmark("Custom encode", runs, Base64.encode, data);
+Util.benchmark("Custom decode", runs, Base64.decode, encoded);
--- src/Util.js
+++ src/Util.js
@@ -2,6 +2,11 @@ import fs from "fs";
 import crypto from "crypto";
 
 const Util = {
+    round: (num, digits) => {
+        const exp = 10 ** digits;
+        return Math.round((num + Number.EPSILON) * exp) / exp;
+    },
+
     encodeUtf8String: str => {
         const encoder = new TextEncoder();
         return encoder.encode(str);
@@ -91,6 +96,43 @@ const Util = {
                 });
             });
         }
+    },
+
+    benchmark: (name, runs, func, ...args) => {
+        let result;
+
+        let min = Number.MAX_VALUE,
+            max = 0,
+            sum = 0;
+
+        for (let i = 1; i <= runs; i++) {
+            const t1 = performance.now();
+
+            const out = func(...args);
+
+            const t2 = performance.now(),
+                time = t2 - t1;
+
+            if (typeof result === "undefined") {
+                result = out;
+            }
+
+            min = Math.min(min, time);
+            max = Math.max(max, time);
+            sum += time;
+        }
+
+        const avg = sum / runs;
+
+        const minStr = Math.round(min * 1000).toLocaleString(),
+            maxStr = Math.round(max * 1000).toLocaleString(),
+            avgStr = Math.round(avg * 1000).toLocaleString(),
+            sumStr = Util.round(sum / 1000, 3);
+
+        console.log(
+            `${name} - ${runs} runs | min: ${minStr}us | max: ${maxStr}us | avg: ${avgStr}us | total: ${sumStr}s`
+        );
+        return [min, max, avg, sum, result];
     }
 };
 
